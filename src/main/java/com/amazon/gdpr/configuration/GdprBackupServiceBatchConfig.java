59c50f61a7e024d6ed58acc366fd1d6026fc746c
Apparao Dasari <dapparao@amazon.com>
2021-04-09 11:32:36 +0530

    commit

--- src/main/java/com/amazon/gdpr/configuration/GdprBackupServiceBatchConfig.java
+++ src/main/java/com/amazon/gdpr/configuration/GdprBackupServiceBatchConfig.java
@@ -244,9 +244,8 @@ public class GdprBackupServiceBatchConfig {
 					System.out.println("tranType::"+tranType);
 					trimmedStrings=lstcls;
 					trimmedStrings.remove("id");
-					
-					trimmedStrings.remove("run_id");
-					trimmedStrings.remove("created_date_time");
+					//trimmedStrings.remove("run_id");
+					//trimmedStrings.remove("created_date_time");
 					}
 				 }
 				Set<String> hSet = new HashSet<String>(trimmedStrings);
@@ -262,8 +261,12 @@ public class GdprBackupServiceBatchConfig {
 				   selectColumns, runId);
 				completeQuery = completeQuery.replaceAll("TAG.", "SF_ARCHIVE.");
 				@SuppressWarnings("unchecked")
-				String backupDataInsertQuery = "INSERT INTO BKP." + backupTableName + " (ID,RUN_ID," + selectColumns + ") "
-						+ completeQuery + " ON CONFLICT ON CONSTRAINT BKP_"+backupTableName.toUpperCase()+"_CHECK DO UPDATE " + "  SET " + splittedValues + ";";
+				String backupDataInsertQuery = "INSERT INTO BKP." + backupTableName + " (ID," + selectColumns + ") "
+						+ completeQuery + " ON CONFLICT (id) DO UPDATE " + "  SET " + splittedValues + ";";
+				
+				//String backupDataInsertQuery = "INSERT INTO BKP." + backupTableName + " (ID,RUN_ID," + selectColumns + ") "
+					//	+ completeQuery + " ON CONFLICT ON CONSTRAINT BKP_"+backupTableName.toUpperCase()+"_CHECK DO UPDATE " + "  SET " + splittedValues + ";";
+				
 				insertcount = backupServiceDaoImpl.insertBackupTable(backupDataInsertQuery);
 
 				System.out.println("Inserted::"+insertcount+"backupDataInsertQuery::::::#$ " +
